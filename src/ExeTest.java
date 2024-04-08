763441974665622d5e5a1640405078cd039f69ca
takaoS <s.takao0710@gmail.com>
2024-04-08 19:44:22 +0900

    [add] ユーザー入力文字列値取得機能の実装
    ExeTest.java で動作確認済み

--- src/ExeTest.java
+++ src/ExeTest.java
@@ -1,3 +1,8 @@
+import util.GameUtils;
+
 public class ExeTest {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String inputStr = GameUtils.getInputString();
+    System.out.println("入力値：" + inputStr);
+  }
  }
--- src/util/GameUtils.java
+++ src/util/GameUtils.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class GameUtils {
   private static final String REGEX_ALPHABET = "^[A-Za-z]{4,12}$"; // アルファベットのみで4~12文字
 
@@ -7,4 +11,26 @@ public class GameUtils {
    * コンストラクタ
    */
   private GameUtils() {}
+
+  /**
+   * コンソールにてユーザーに入力を求め、その入力された文字列の値を取得する。
+   * 
+   * @return 入力値
+   */
+  public static String getInputString() {
+    String inputStr = null;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("文字列を入力してください");
+    try {
+      inputStr = br.readLine();
+
+    } catch (IOException e) {
+      e.getStackTrace();
+      System.out.println("入力エラー…再度 入力してください");
+      getInputString();
+    }
+
+    return inputStr;
+  }
 }
