71f309d7bf7e8615b86bd1273ca2c6fe2b773977
takaoS <s.takao0710@gmail.com>
2024-04-08 19:22:55 +0900

    [add] GameUtilsクラスの作成
    定数と、空のコンストラクタを定義

---
+++ src/util/GameUtils.java
@@ -0,0 +1,10 @@
+package util;
+
+public class GameUtils {
+  private static final String REGEX_ALPHABET = "^[A-Za-z]{4,12}$"; // アルファベットのみで4~12文字
+
+  /**
+   * コンストラクタ
+   */
+  private GameUtils() {}
+}
