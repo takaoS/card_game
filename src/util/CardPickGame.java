f448ae374c50d8cbbc558f4e5afc891dbf0c4577
takaoS <s.takao0710@gmail.com>
2024-04-09 16:10:23 +0900

    [add] カード合計値取得機能の実装

--- src/util/CardPickGame.java
+++ src/util/CardPickGame.java
@@ -13,4 +13,30 @@ public class CardPickGame {
   public CardPickGame(int _possessionCoin) {
     this.possessionCoin = _possessionCoin;
   }
+
+  /**
+   * 1〜10を1組とした2つのカードデッキから、それぞれ1枚カードを引き、その結果と合計値を表示。また、その合計値を返却する。
+   * 
+   * @return 引いた2枚のカードの合計値
+   */
+  public int getCard() {
+    int maxCardValue = 10;
+    int[][] deck = new int[deckSetCount][maxCardValue];
+
+    // 1〜10を1組とし、deckSetCount の数分の組で構成されたデッキを生成する
+    for (int i=0; i<deckSetCount; i++) {
+      for (int j=0; j<maxCardValue; j++) {
+        deck[i][j] = j + 1;
+      }
+    }
+
+    // カードを1枚ずつ、それぞれのデッキから引く
+    int cardA = deck[0][GameUtils.getRandomInt(maxCardValue-1)];
+    int cardB = deck[1][GameUtils.getRandomInt(maxCardValue-1)];
+
+    // 引いたそれぞれのカードと合計値を出力し、合計値を返す
+    int total = cardA + cardB;
+    System.out.println("引いたカード：" + cardA + " & " + cardB + " | 合計値：" + total);
+    return total;
+  }
 }
