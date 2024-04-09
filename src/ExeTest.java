013eb138f4c7e02e859ad19f42782fdb9cf2103d
takaoS <s.takao0710@gmail.com>
2024-04-09 11:00:41 +0900

    [add] 乱数取得機能の実装
    動作確認として、30を最大値とする乱数を生成するよう ExeTest.java を更新

--- src/ExeTest.java
+++ src/ExeTest.java
@@ -2,7 +2,7 @@ import util.GameUtils;
 
 public class ExeTest {
   public static void main(String[] args) {
-    Integer inputInt = GameUtils.getInputInt();
-    System.out.println("入力値：" + inputInt);
+    Integer randomInt = GameUtils.getRandomInt(30);
+    System.out.println("乱数：" + randomInt);
   }
  }
--- src/util/GameUtils.java
+++ src/util/GameUtils.java
@@ -3,6 +3,7 @@ package util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 public class GameUtils {
   private static final String REGEX_ALPHABET = "^[A-Za-z]{4,12}$"; // アルファベットのみで4~12文字
@@ -53,5 +54,17 @@ public class GameUtils {
     }
 
     return inputInt;
+  }
+
+  /**
+   * 整数値の乱数を生成し返却する。
+   * 
+   * @param maxValue 生成する乱数の最大値となる整数
+   * @return 生成された乱数
+   */
+  public static int getRandomInt(int maxValue) {
+    Random random = new Random();
+    int randomInt = random.nextInt(maxValue);
+    return randomInt;
   }
 }
