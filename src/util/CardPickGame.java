ee1f7c1f6706b4929b392c2951a56d6bc3cae1d0
takaoS <s.takao0710@gmail.com>
2024-04-09 16:43:01 +0900

    [add] カード合計値判定機能の実装
    引いた2枚のカードの合計値が11以上の場合に「勝ち」となるような機能を実装し、ゲーム実行用メソッドで結果を出力するよう修正。また、getCard() メソッド内の、カードの番号の最大値を保持する変数 maxCardValue を、定数 MAX_CARD_VALUE に修正

--- src/util/CardPickGame.java
+++ src/util/CardPickGame.java
@@ -15,7 +15,10 @@ public class CardPickGame {
   }
 
   public void execute() {
-    getCard();
+    boolean result = judgeCard(getCard());
+
+    if (result) System.out.println("判定：勝ち");
+    else System.out.println("判定：負け");
   }
 
   /**
@@ -24,23 +27,36 @@ public class CardPickGame {
    * @return 引いた2枚のカードの合計値
    */
   private int getCard() {
-    int maxCardValue = 10;
-    int[][] deck = new int[deckSetCount][maxCardValue];
+    final int MAX_CARD_VALUE = 10; // カードの番号の最大値
+    int[][] deck = new int[deckSetCount][MAX_CARD_VALUE];
 
     // 1〜10を1組とし、deckSetCount の数分の組で構成されたデッキを生成する
     for (int i=0; i<deckSetCount; i++) {
-      for (int j=0; j<maxCardValue; j++) {
+      for (int j=0; j<MAX_CARD_VALUE; j++) {
         deck[i][j] = j + 1;
       }
     }
 
     // カードを1枚ずつ、それぞれのデッキから引く
-    int cardA = deck[0][GameUtils.getRandomInt(maxCardValue)];
-    int cardB = deck[1][GameUtils.getRandomInt(maxCardValue)];
+    int cardA = deck[0][GameUtils.getRandomInt(MAX_CARD_VALUE)];
+    int cardB = deck[1][GameUtils.getRandomInt(MAX_CARD_VALUE)];
 
     // 引いたそれぞれのカードと合計値を出力し、合計値を返す
     int total = cardA + cardB;
     System.out.println("引いたカード：" + cardA + " & " + cardB + " | 合計値：" + total);
     return total;
+  }
+
+  /**
+   * 引数の値 (カード合計値) を利用し勝敗を判定する。
+   * 
+   * @param getCardResult カード合計値
+   * @return 勝敗判定
+   */
+  private boolean judgeCard(int getCardResult) {
+    final int WINNING_THRESHOLD = 11; // 勝利条件：カードの合計値が11以上
+
+    if (getCardResult < WINNING_THRESHOLD) return false; // 負け
+    else return true; // 勝ち
   }
 }
