88e4e8325f963538832c5f09fc783927fe76f4de
takaoS <s.takao0710@gmail.com>
2024-04-13 17:30:08 +0900

    [add] ゲーム実行＆結果表示機能の実装
    ユーザー名の入力完了後、ゲームを実行し、勝敗に応じて所持コインを更新する機能を実装。ただし、所持コインが更新されないバグが発生しているため、次回修正予定

--- src/Playing.java
+++ src/Playing.java
@@ -1,3 +1,4 @@
+import util.CardPickGame;
 import util.GameUtils;
 
 public class Playing {
@@ -8,9 +9,12 @@ public class Playing {
    */
   public static void main(String[] args) {
     String username = "";
+    int possessionCoin = 1000;
 
+    // ゲーム開始
     System.out.println("ようこそ！");
 
+    // ユーザー名の入力
     System.out.println("ユーザー名を入力してください");
     boolean checkResult;
     do {
@@ -19,6 +23,14 @@ public class Playing {
       if (!checkResult) System.out.println("4〜12文字の半角アルファベットで入力してください");
     } while (!checkResult);
 
+    // ユーザー名の入力完了
     System.out.println(username + "さん、こんにちは！");
+
+    // ゲームを実行し、所持コインを更新
+    CardPickGame objCardPickGame = new CardPickGame(possessionCoin);
+    possessionCoin = objCardPickGame.execute();
+
+    // ゲーム終了 (結果表示)
+    System.out.println(username + "さんの現在の所持コイン：" + possessionCoin + "枚");
   }
 }
