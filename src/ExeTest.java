5555203ec216046462e2ed15a30cfe765e1f7036
takaoS <s.takao0710@gmail.com>
2024-04-15 10:52:26 +0900

    [add] カード取得機能の実装
    ExeTest.java でテスト済み

--- src/ExeTest.java
+++ src/ExeTest.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import util.CardPickGame;
 import util.GameUtils;
+import util.HighAndLowGame;
 
 public class ExeTest {
   public static void main(String[] args) {
-    
+    HighAndLowGame obj = new HighAndLowGame(100, 2);
+
+    List<Integer> cardList = new ArrayList<Integer>();
+
+    cardList = obj.getCard(cardList);
+    System.out.println(cardList);
+
+    cardList = obj.getCard(cardList);
+    System.out.println(cardList);
+
+    // カードセットは 2つに設定しているので、引けないはず
+    cardList = obj.getCard(cardList);
+    System.out.println(cardList);
   }
  }
--- src/util/HighAndLowGame.java
+++ src/util/HighAndLowGame.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.List;
+
 public class HighAndLowGame {
   private int earnedCoinCount;    // 獲得コイン枚数
   private int maxWinCoin = 10000; // 最大獲得コイン数
@@ -14,5 +16,35 @@ public class HighAndLowGame {
   public HighAndLowGame(int _earnedCoinCount, int _deckSetCount) {
     this.earnedCoinCount = _earnedCoinCount;
     this.deckSetCount = _deckSetCount;
+  }
+
+  /**
+   * カードセットの中から、ランダムにカードを1枚引く。
+   * 
+   * @param cardList 現在の引いたカードリスト
+   * @return カードリスト
+   */
+  public List<Integer> getCard(List<Integer> cardList) {
+    // カードは、それぞれのカードセットから1枚ずつしか引けない
+    // そのため、引いたカードの合計枚数が、カードセット数よりも多くなってはダメ
+    if (cardList.size() + 1 <= deckSetCount) {
+      final int MAX_CARD_VALUE = 10; // カードの番号の最大値
+
+      // カードを1枚、カードセットから引く
+      int cardA = 0;
+      while (cardA == 0) { // カードの番号に 0 は存在しないため
+        cardA = GameUtils.getRandomInt(MAX_CARD_VALUE);
+      }
+      cardList.add(cardA);
+
+      // 引いたカードを表示する
+      int lastIdx = cardList.size()-1;
+      System.out.println("引いたカードの番号：" + cardList.get(lastIdx));
+
+    } else {
+      System.out.println("それぞれのカードセットからカードを引き終わっているため、これ以上引けません");
+    }
+
+    return cardList;
   }
 }
